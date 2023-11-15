1cc49c9cc8daa3a34ce4d7d5f3dee598f2de2c7a
Harrison Townend <harrison.townend1@studytafensw.edu.au>
2023-11-15 21:10:59 +1100

    adding dbContext

---
+++ Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test_api.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    [System.Text.Json.Serialization.JsonIgnore]
+    public virtual ICollection<Person> People { get; set; } = new List<Person>();
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using test_api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RoiDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
